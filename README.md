e933141f4f24236cdf84fed52788f7ecc7d7d946
Gediminas Dausynas <gediminas.dausynas@gmail.com>
2021-07-03 20:45:28 +0300

    New commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Getting Started
+```bash
+yarn add @geedium/editor [OR] npm i -g @geedium/editor
+```
+
+# Vue Component
+```vue
+import Editor from '@geedium/editor';
+
+export default {
+    components: {
+        Editor
+    }
+}
+```
--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,9 +1,57 @@
 <template>
-    <input type="text" />
+  <div class="box">
+    <div class="toolbar">
+      <button>B</button>
+      <button>U</button>
+      <button>I</button>
+    </div>
+    <div class="viewport">
+      <p class="viewport--p">⠀</p>
+    </div>
+  </div>
 </template>
 
-<<script>
+<script>
 export default {
-    name: 'Editor'
-}
+  name: "Editor",
+};
 </script>
+
+<style scoped>
+.box {
+  box-shadow: 1px 2px 5px #c6c6c6;
+  min-height: 150px;
+  display: inline-block;
+  min-width: 400px;
+  overflow: hidden;
+}
+.toolbar {
+  background: #fbfbfb;
+  min-height: 32px;
+  max-height: 40px;
+  box-shadow: 0px 1px 3px 0px #e6e6e6;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 4px;
+}
+.toolbar button {
+  width: 32px;
+  height: 32px;
+}
+.viewport {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 40px;
+  text-align: left;
+  overflow-y: scroll;
+  overflow-wrap: break-word;
+}
+.viewport:hover {
+  cursor: text;
+}
+.viewport--p {
+  display: inline;
+}
+</style>
